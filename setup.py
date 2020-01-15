419706166b0f316c4e734b284c56a0b549318dea
AnTengye <32085150+AnTengye@users.noreply.github.com>
2020-01-15 16:15:03 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(name='circus-web',
           "Development Status :: 3 - Alpha"],
       install_requires=install_requires,
       dependency_links=[
-          'https://github.com/paulocheque/tornadio2/archive/master.zip',
+          'https://github.com/paulocheque/tornadio2/archive/master.zip#egg=tornadIO2-0.0.3',
       ],
       tests_require=['webtest', 'unittest2'],
       test_suite='circusweb.tests',
